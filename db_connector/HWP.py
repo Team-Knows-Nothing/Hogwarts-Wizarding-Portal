dc1d66fc24cf9c9f722a60860ba17115af79c37e
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-05-27 03:30:10 -0700

    Added INSERT functionalities for all pages

--- db_connector/HWP.py
+++ db_connector/HWP.py
@@ -19,7 +19,13 @@ def index():
 @app.route('/index', methods = ['POST', 'GET'])
 def show_students():
 	db_connection = connect_to_database()
-	if request.method == 'POST':
+	if request.method == 'GET':
+		print("Executing a sample query on the database using the credentials from db_credentials.py")
+		query = 'SELECT ID, Student_Lname, Student_Fname, Student_Birthdate, Student_Year,(SELECT House_Name FROM Houses WHERE Students.House_ID = Houses.ID) FROM Students ORDER BY `Student_Lname` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print("GET FOR INDEX PAGE",result)
+		return render_template('index.html', rows=result)
+	elif request.method == 'POST':
 		# Print actions to terminal
 		print("Adding a student to the database")
 
@@ -29,13 +35,6 @@ def show_students():
 		birthdate = request.form['birthdate_input']
 		year = request.form['year_input']
 		house = request.form['house_input']
-
-		print("POST FOR INDEX PAGE")
-		print(first_name)
-		print(last_name)
-		print(birthdate)
-		print(year)
-		print(house) #TEST: print inputted house
 
 		if house == "Gryffindor":
 			house = 1
@@ -56,54 +55,147 @@ def show_students():
 		print("GET FOR INDEX PAGE",result)
 		return render_template('index.html', rows=result)
 
-	elif request.method == 'GET':
-		print("Executing a sample query on the database using the credentials from db_credentials.py")
-		query = 'SELECT ID, Student_Lname, Student_Fname, Student_Birthdate, Student_Year,(SELECT House_Name FROM Houses WHERE Students.House_ID = Houses.ID) FROM Students ORDER BY `Student_Lname` ASC;'
-		result = execute_query(db_connection, query).fetchall()
-		print("GET FOR INDEX PAGE",result)
-		return render_template('index.html', rows=result)
-# Display (READ/SELECT) Houses in database
-@app.route('/Houses')
-def show_houses():
 
-	print('Showing all Houses in database')
+# Display (READ/SELECT) Houses in database
+@app.route('/Houses', methods = ['POST', 'GET'])
+def show_houses():
 	db_connection = connect_to_database()
-	query = 'SELECT House_Name, House_Mascot, House_Founder, House_Head FROM Houses;'
-	result = execute_query(db_connection, query).fetchall()
-	print(result)
-	return render_template('houses.html', rows=result)
+	if request.method == 'GET':
+		print('Showing all Houses in database')
+		query = 'SELECT House_Name, House_Mascot, House_Founder, House_Head FROM Houses;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('houses.html', rows=result)
+	elif request.method == 'POST':
+		# Print actions to terminal
+		print("Added a House to the database")
+
+		# Gather input fields into variables
+		house_name = request.form['house_name_input']
+		house_mascot = request.form['house_mascot_input']
+		house_founder = request.form['house_founder_input']
+		house_head = request.form['house_head_input']
+
+		# Insert Input variables into database
+		query = 'INSERT INTO Houses (House_Name,House_Mascot,House_Founder,House_Head) VALUES (%s,%s,%s,%s)'
+		data = (house_name,house_mascot,house_founder,house_head)
+		execute_query(db_connection, query, data)
+
+		query = 'SELECT House_Name, House_Mascot, House_Founder, House_Head FROM Houses;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('houses.html', rows=result)
+	
 
 # Display (READ/SELECT) Professors in database
-@app.route('/Professors')
+@app.route('/Professors', methods = ['POST', 'GET'])
 def show_professors():
-	print('Showing all Professors in database')
 	db_connection = connect_to_database()
-	query = 'SELECT Professor_Lname,Professor_Fname,(SELECT House_Name FROM Houses WHERE Professors.House_ID = Houses.ID)FROM `Professors` ORDER BY `Professor_Lname` ASC;'
-	result = execute_query(db_connection, query).fetchall()
-	print(result)
-	return render_template('professors.html', rows=result)
+	if request.method == 'GET':
+		print('Showing all Professors in database')
+		db_connection = connect_to_database()
+		query = 'SELECT ID, Professor_Lname,Professor_Fname,(SELECT House_Name FROM Houses WHERE Professors.House_ID = Houses.ID)FROM `Professors` ORDER BY `Professor_Lname` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('professors.html', rows=result)
+	elif request.method == 'POST':
+		# Print actions to terminal
+		print("Adding a professor to the database")
+
+		# Gather input fields into variables
+		last_name = request.form['last_name_input']
+		first_name = request.form['first_name_input']
+		house = request.form['house_input']
+
+		if house == "Gryffindor":
+			house = 1
+		elif house == "Slytherin":
+			house = 2
+		elif house == "Hufflepuff":
+			house = 3
+		elif house == "Ravenclaw":
+			house = 4
+
+		# Insert Input variables into database
+		query = 'INSERT INTO Professors (Professor_Fname,Professor_Lname,House_ID) VALUES (%s,%s,%s)'
+		data = (last_name,first_name,house)
+		execute_query(db_connection, query, data)
+
+		query = 'SELECT ID, Professor_Lname,Professor_Fname,(SELECT House_Name FROM Houses WHERE Professors.House_ID = Houses.ID)FROM `Professors` ORDER BY `Professor_Lname` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('professors.html', rows=result)
 
 # Display (READ/SELECT) Classes in database
-@app.route('/Classes')
+@app.route('/Classes', methods = ['POST', 'GET'])
 def show_classes():
-	print('Showing all Classes in database')
 	db_connection = connect_to_database()
-	query = 'SELECT ID, Class_Name, Class_Credit, (SELECT Professor_Lname FROM Professors WHERE Classes.Professor_ID = Professors.ID) FROM `Classes` ORDER BY `Class_Name` ASC;'
-	result = execute_query(db_connection, query).fetchall()
-	print(result)
-	return render_template('classes.html', rows=result)
+	if request.method == 'GET':	
+		print('Showing all Classes in database')
+		query = 'SELECT ID, Class_Name, Class_Credit, (SELECT Professor_Lname FROM Professors WHERE Classes.Professor_ID = Professors.ID) FROM `Classes` ORDER BY `Class_Name` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('classes.html', rows=result)
+	elif request.method == 'POST':
+		# Print actions to terminal
+		print("Added a class to the database")
+
+		# Gather input fields into variables
+		class_title = request.form['class_title_input']
+		class_credits = request.form['class_credits_input']
+		professor = request.form['class_professor_input']
+
+		# Insert Input variables into database
+		query = 'INSERT INTO Classes (Class_Name,Class_Credit,Professor_ID) VALUES (%s,%s,%s)'
+		data = (class_title,class_credits,professor)
+		execute_query(db_connection, query, data)
+
+		query = 'SELECT ID, Class_Name, Class_Credit, (SELECT Professor_Lname FROM Professors WHERE Classes.Professor_ID = Professors.ID) FROM `Classes` ORDER BY `Class_Name` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('classes.html', rows=result)
+		
 
 # Display (READ/SELECT) Registrations in database
-@app.route('/Registrations')
+@app.route('/Registrations', methods = ['POST', 'GET'])
 
 def show_registrations():
-
-	print('Showing all Registrations in database')
 	db_connection = connect_to_database()
-	query = 'SELECT Student_ID,(SELECT Student_LName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `Last Name` , (SELECT Student_FName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `First Name` ,Class_ID, (SELECT Class_Name FROM Classes WHERE Registrations.Class_ID = Classes.ID) FROM `Registrations` ORDER BY `Last Name` ASC;'
-	result = execute_query(db_connection, query).fetchall()
-	print(result)
-	return render_template('registrations.html', rows=result)
+	if request.method == 'GET':	
+		print('Showing all Registrations in database')
+		query = 'SELECT Student_ID,(SELECT Student_LName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `Last Name` , (SELECT Student_FName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `First Name` ,Class_ID, (SELECT Class_Name FROM Classes WHERE Registrations.Class_ID = Classes.ID) FROM `Registrations` ORDER BY `Last Name` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('registrations.html', rows=result)
+	elif request.method == 'POST':	
+		# Print actions to terminal
+		print("Added a registration to the database")
+
+		# Gather input fields into variables
+		student_id = request.form['student_id_input']
+		class_id = request.form['class_id_input']
+
+		# Insert Input variables into database
+		query = 'INSERT INTO Registrations (Student_ID, Class_ID)  VALUES (%s,%s)'
+		data = (student_id,class_id)
+		execute_query(db_connection, query, data)
+
+		query = 'SELECT Student_ID,(SELECT Student_LName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `Last Name` , (SELECT Student_FName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `First Name` ,Class_ID, (SELECT Class_Name FROM Classes WHERE Registrations.Class_ID = Classes.ID) FROM `Registrations` ORDER BY `Last Name` ASC;'
+		result = execute_query(db_connection, query).fetchall()
+		print(result)
+		return render_template('registrations.html', rows=result)
+		
+
+		# elif student_ID_search:
+		# 	query = 'SELECT Student_ID,(SELECT Student_LName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `Last Name` , (SELECT Student_FName FROM Students WHERE Registrations.Student_ID = Students.ID) AS `First Name` ,Class_ID, (SELECT Class_Name FROM Classes WHERE Registrations.Class_ID = Classes.ID) FROM `Registrations` WHERE Student_ID = (%s) ORDER BY `Last Name` ASC;'
+		# 	data = student_ID_search
+		# 	result = execute_query(db_connection, query,data).fetchall()
+			
+		# 	print(result)
+		# 	return render_template('registrations.html', rows=result)
+
+
+
 
 
 
