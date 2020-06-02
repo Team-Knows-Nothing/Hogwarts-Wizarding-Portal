d3dc62a87b6d25749a304c2be91a96e4fdd96112
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-06-01 23:30:38 -0700

    UPDATE functionality for all pages

--- db_connector/HWP.py
+++ db_connector/HWP.py
@@ -4,18 +4,15 @@ from db_connector.db_connector import connect_to_database, execute_query
 
 app = Flask(__name__)
 
-#the route is what you will type in browser
-@app.route('/hello')
-#the name of this function is just a cosmetic thing
-def hello():
-    #this is the output returned to browser
-    return "Hello worldDDDDD!"
 
+#route typed in browser
 @app.route('/')
-def index():
-    return "<i>Are you looking for /db-test or /hello ?</i>"
+#cosmetic name of function
+def homepage():
+	# output html, render html page
+    return "<i>Hogwarts Wizarding Portal ?</i>"
 
-# Display Students
+#  Students Page
 @app.route('/index', methods = ['POST', 'GET'])
 def show_students():
 	db_connection = connect_to_database()
@@ -72,6 +69,45 @@ def show_students():
 		return render_template('index.html', rows=result)
 
 
+# UPDATE students
+@app.route('/update_students/<int:id>', methods=['POST','GET'])
+def update_student(id):
+	print('Updating student')
+	db_connection = connect_to_database()
+	if request.method == 'GET':
+		print('The GET request')
+		student_query = 'SELECT ID, Student_Fname, Student_Lname, Student_Birthdate, Student_year from Students WHERE id = %s' % (id)
+		student_result = execute_query(db_connection, student_query).fetchone()
+		# query for FK columns
+		house_query = 'SELECT ID, House_Name from Houses'
+		house_results = execute_query(db_connection, house_query).fetchall()
+		print('Returning')
+		return render_template('update_students.html', house = house_results, student = student_result)
+	elif request.method == 'POST':
+		print('The POST request')
+		first_name = request.form['first_name_input']
+		last_name = request.form['last_name_input']
+		birthday = request.form['birthdate_input']
+		year = request.form['year_input']
+		house = request.form['house_input']
+
+
+		if house == "Gryffindor":
+			house = 1
+		elif house == "Slytherin":
+			house = 2
+		elif house == "Hufflepuff":
+			house = 3
+		elif house == "Ravenclaw":
+			house = 4
+
+		query = "UPDATE Students SET Student_Fname = %s, Student_Lname = %s, Student_Birthdate = %s, Student_Year = %s ,House_ID = %s WHERE id = %s"		
+		data = (first_name, last_name, birthday, year, house, id)
+		result = execute_query(db_connection, query, data)
+		print(str(result.rowcount) + " row(s) updated")
+
+		return redirect('/index')
+
 # Display (READ/SELECT) Houses in database
 @app.route('/Houses', methods = ['POST', 'GET'])
 def show_houses():
@@ -117,6 +153,35 @@ def show_houses():
 		result = execute_query(db_connection, query).fetchall()
 		print(result)
 		return render_template('houses.html', rows=result)
+
+# UPDATE houses
+@app.route('/update_houses/<int:id>', methods=['POST','GET'])
+def update_house(id):
+	print('Updating house')
+	db_connection = connect_to_database()
+	if request.method == 'GET':
+		print('The GET request')
+		house_query = 'SELECT ID, House_Name, House_Mascot, House_Founder, House_Head from Houses WHERE id = %s' % (id)
+		house_results = execute_query(db_connection, house_query).fetchone()
+		print('Returning')
+		return render_template('update_houses.html', house = house_results)
+	elif request.method == 'POST':
+		print('The POST request')
+		name = request.form['house_name_input']
+		mascot = request.form['house_mascot_input']
+		founder = request.form['house_founder_input']
+		head = request.form['house_head_input']
+		
+
+
+
+		query = "UPDATE Houses SET House_Name = %s, House_Mascot = %s, House_Founder = %s, House_Head = %s WHERE id = %s"		
+		data = (name, mascot, founder, head, id)
+		result = execute_query(db_connection, query, data)
+		print(str(result.rowcount) + " row(s) updated")
+		
+		return redirect('/Houses')
+
 
 # Display (READ/SELECT) Professors in database
 @app.route('/Professors', methods = ['POST', 'GET'])
@@ -174,6 +239,39 @@ def show_professors():
 		print(result)
 		return render_template('professors.html', rows=result)
 
+
+@app.route('/update_professors/<int:id>', methods=['POST','GET'])
+def update_professor(id):
+	print('Updating professor')
+	db_connection = connect_to_database()
+	if request.method == 'GET':
+		print('The GET request')
+		professor_query = 'SELECT ID, Professor_Fname, Professor_Lname, House_ID from Professors WHERE id = %s' % (id)
+		professor_result = execute_query(db_connection,professor_query).fetchone()
+		print('Returning')
+		return render_template('update_professors.html', professor = professor_result)
+	elif request.method == 'POST':
+		print('The POST request')
+		first_name = request.form['first_name_input']
+		last_name = request.form['last_name_input']
+		house = request.form['house_input']
+		if house == "Gryffindor":
+			house = 1
+		elif house == "Slytherin":
+			house = 2
+		elif house == "Hufflepuff":
+			house = 3
+		elif house == "Ravenclaw":
+			house = 4
+		elif house == "None":
+			house = None
+
+		query = "UPDATE Professors SET Professor_Fname = %s, Professor_Lname = %s, House_ID = %s WHERE id = %s"		
+		data = (first_name, last_name, house, id)
+		result = execute_query(db_connection, query, data)
+		print(str(result.rowcount) + " row(s) updated")
+		return redirect('/Professors')
+
 # Display (READ/SELECT) Classes in database
 @app.route('/Classes', methods = ['POST', 'GET'])
 def show_classes():
@@ -217,6 +315,28 @@ def show_classes():
 		result = execute_query(db_connection, query).fetchall()
 		print(result)
 		return render_template('classes.html', rows=result)
+
+#display update form and process any updates, using the same function
+@app.route('/update_classes/<int:id>', methods=['POST','GET'])
+def update_class(id):
+	print('Updating class')
+	db_connection = connect_to_database()
+	if request.method == 'GET':
+		print('The GET request')
+		classes_query = 'SELECT ID, Class_Name, Class_Credit, Professor_ID from Classes WHERE id = %s' % (id)
+		classes_result = execute_query(db_connection,classes_query).fetchone()
+		print('Returning')
+		return render_template('update_classes.html', classes = classes_result)
+	elif request.method == 'POST':
+		print('The POST request')
+		class_name = request.form['class_title_input']
+		class_credit = request.form['class_credits_input']
+		professor = request.form['class_professor_input']
+		query = "UPDATE Classes SET Class_Name = %s, Class_Credit = %s, Professor_ID = %s WHERE id = %s"		
+		data = (class_name, class_credit, professor, id)
+		result = execute_query(db_connection, query, data)
+		print(str(result.rowcount) + " row(s) updated")
+		return redirect('/Classes')
 		
 
 # Display (READ/SELECT) Registrations in database
